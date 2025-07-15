89c6e7c228bea915a7d45030705416606a418bf0
Oleksii Diachenko <diachenko30032001@gmail.com>
2025-07-15 12:25:52 +0200

    modified:   src/app/dashboard/dashboard-item/dashboard-item.component.css 	modified:   src/app/dashboard/dashboard-item/dashboard-item.component.html 	modified:   src/app/dashboard/dashboard-item/dashboard-item.component.ts 	modified:   src/app/dashboard/server-status/server-status.component.css 	modified:   src/app/dashboard/server-status/server-status.component.html 	modified:   src/app/dashboard/server-status/server-status.component.ts 	modified:   src/app/dashboard/tickets/new-ticket/new-ticket.component.html 	modified:   src/app/dashboard/tickets/new-ticket/new-ticket.component.ts 	modified:   src/app/dashboard/tickets/tickets.component.css 	modified:   src/app/dashboard/tickets/tickets.component.html 	modified:   src/app/dashboard/traffic/traffic.component.css 	modified:   src/app/dashboard/traffic/traffic.component.html 	modified:   src/app/shared/control/control.component.html 	modified:   src/app/shared/control/control.component.ts

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit, OnDestroy {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private interval?: ReturnType<typeof setInterval>;
+
+  ngOnInit() { 
+    this.interval = setInterval(() => {
+      const rnd = Math.random();
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000)
+  }
+  
+  ngAfterViewInit() {
+    console.log('After view init');
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
 }
